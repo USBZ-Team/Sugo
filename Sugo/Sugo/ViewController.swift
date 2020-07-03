e80a1d89667a47b4d335c28af207e99aa2434de6
USBZ-Team <67809048+USBZ-Team@users.noreply.github.com>
2020-07-04 01:54:59 +0200

    lolol

---
+++ Sugo/Sugo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sugo
+//
+//  Created by Gianni Casto on 04/07/2020.
+//  Copyright © 2020 USBZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
